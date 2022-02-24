748d939debe5d485459350f9d69633be75db4028
surendhar-1706 <surendhar2k18@gmail.com>
2022-02-24 17:06:59 +0530

    working dj_next_auth

---
+++ 2022/Project3/backend/backend/accounts/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.db.models.deletion import CASCADE
+# Create your models here.
---
+++ 2022/Project3/backend/backend/accounts/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path,include
+
+urlpatterns = [
+ path('auth/', include('dj_rest_auth.urls')),
+ path('auth/registration/', include('dj_rest_auth.registration.urls')),
+]
+
+
