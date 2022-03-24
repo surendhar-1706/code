a55b90cc5840b5112140290925f89a7f0fca1165
surendhar-palanisamy <surendhar2k18@gmail.com>
2022-03-25 00:08:27 +0530

    sty

---
+++ 2022/YourCar/frontend/components/home/Home.tsx
@@ -0,0 +1,10 @@
+import { Heading, Text } from '@chakra-ui/react'
+import React from 'react'
+
+function HomeComponent() {
+    return (
+        <Heading>HomeComponentpage</Heading>
+    )
+}
+
+export default HomeComponent
