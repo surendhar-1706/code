29564623b77ea9211e039077942e25209df88e6c
surendhar-palanisamy <surendhar2k18@gmail.com>
2022-04-04 17:43:20 +0530

    home page

--- 2022/animations/nftlandingpage/src/components/sections/Home.js
+++ 2022/animations/nftlandingpage/src/components/sections/Home.js
@@ -1,7 +1,106 @@
+import { Typography, Button, Box } from "@mui/material";
 import React from "react";
-
+import Typewriter from "typewriter-effect";
+import RoundTextBlack from "../../assets/Rounded.png";
+import nftvideo from "../../assets/Home Video.mp4";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <Box
+        sx={{
+          height: "80vh",
+          width: "100vw",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+
+          px: 10,
+        }}
+      >
+        <Box
+          sx={{
+            height: "50%",
+            width: "38%",
+          }}
+        >
+          <Typography sx={{ fontFamily: "cursive" }} fontSize={48}>
+            Discover A New Era Of Cool
+          </Typography>
+          <Box
+            sx={{
+              fontSize: 48,
+              fontFamily: "Akaya Telivigala",
+              width: "50%",
+            }}
+          >
+            <Typewriter
+              options={{
+                autoStart: true,
+                loop: true,
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString(
+                    `<span style="color: #FF0000;">APE KILLERS!</span>`
+                  )
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString(`<span style="color:#0000FF;">NFTS.</span>`)
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString(
+                    `<span style="color:#FF8C00;">COLLECTIBLE ITEMS.</span>`
+                  )
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .start();
+              }}
+            />
+          </Box>
+          <Typography sx={{ fontFamily: "Akaya Telivigala", fontSize: 24 }}>
+            Bored Of Apes? Try Something New.
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{
+              "&:hover": {
+                background: "black",
+                transform: "scale(0.9)",
+              },
+              backgroundColor: "black",
+              borderRadius: 20,
+              px: 4,
+              py: 1,
+              transition: "ease 0.3s",
+              textTransform: "none",
+              letterSpacing: 3,
+              my: 2,
+            }}
+          >
+            Explore
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            height: "100%",
+            width: "60%",
+          }}
+        >
+          <video
+            height={"100%"}
+            width={"100%"}
+            src={nftvideo}
+            autoPlay
+            muted
+            loop
+          ></video>
+        </Box>
+      </Box>
+      <Box sx={{}}>
+        <img height={100} width={100} alt="" src={RoundTextBlack} />
+      </Box>
+    </Box>
+  );
 }
 
 export default Home;
