98809a83cbd38326c3ccaab93fe6d395b48d2781
surendhar-palanisamy <surendhar2k18@gmail.com>
2022-07-22 23:08:49 +0530

    touches

--- 2022/polish/test/pages/_app.tsx
+++ 2022/polish/test/pages/_app.tsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux'
 import { store } from '../app/store'
 import { QueryClient, QueryClientProvider, } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import '../styles/parser.css'
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
--- 2022/polish/test/pages/crypto/[id].tsx
+++ 2022/polish/test/pages/crypto/[id].tsx
@@ -1,5 +1,8 @@
-import { Box, HStack, Select, Stack, Text } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Box, Divider, Grid, GridItem, HStack, Link, Select, Stack, Text, VStack } from '@chakra-ui/react'
+import HTMLReactParser from 'html-react-parser'
 import millify from 'millify'
+
 import Router, { useRouter } from 'next/router'
 import React, { useState } from 'react'
 import Cryptochart from '../../components/Crypto/Cryptochart'
@@ -26,74 +29,105 @@ function Cryptodetail() {
                     )
                 })}
             </Select>
-
-            <Stack>
-                <Text>Bitcoin Value Statistics</Text>
-                <Text>An overview showing the stats of the Bitcoin</Text>
-                <HStack>
-                    <Text>Price to USD</Text>
-                    <Text>{millify(data.data.coin.price)}</Text>
-                </HStack>
-                <HStack>
-                    <Text>Rank</Text>
-                    <Text>{millify(data.data.coin.rank)}</Text>
-                </HStack>
-                <HStack>
-                    <Text>All Time High</Text>
-                    <Text>{millify(data.data.coin.allTimeHigh.price)}</Text>
-                </HStack>
-
-                <HStack>
-                    <Text>Market Cap</Text>
-                    <Text>{millify(data.data.coin.marketCap)}</Text>
-                </HStack>
-
-            </Stack>
-            <Stack>
-                <Text>Other Statistics</Text>
-                <Text>An overview showing the stats of the Bitcoin</Text>
-                <HStack>
-                    <Text>Number of Exchanges</Text>
-                    <Text>{millify(data.data.coin.numberOfExchanges)}</Text>
-                </HStack>
-                <HStack>
-                    <Text>Number of Markets</Text>
-                    <Text>{millify(data.data.coin.numberOfMarkets)}</Text>
-                </HStack>
-                <HStack>
-                    <Text>Confirmed Supply</Text>
-                    <Text>{data.data.coin.supply.confirmed ? <div>Yes</div> : <div>No</div>}</Text>
-                </HStack>
-                <HStack>
-                    <Text>Circulating Supply</Text>
-                    <Text>{millify(data.data.coin.supply.circulating)}</Text>
-                </HStack>
-                <HStack>
-                    <Text>Total Supply</Text>
-                    <Text>{millify(data.data.coin.supply.total)}</Text>
-                </HStack>
-                <Stack>
-                    <Text>Coin Details</Text>
-                    <Text>{data.data.coin.description}</Text>
-                </Stack>
-
-                <Stack>
-                    <Text>Links</Text>
-                    {data?.data.coin.links.map((link: any) => {
-                        return (
-                            <HStack justifyContent={'space-around'}
-
-                            >
-                                <Text>      {link.type}</Text>
-                                <Text>  {link.name}</Text>
-
+            <Grid rowGap={12} justifyContent={'space-between'} templateColumns='repeat(2, 1fr)' >
+                <GridItem colSpan={1} >
+                    <VStack>
+                        <Stack gap={4}>
+                            <Stack gap={0} py={1}>
+                                <Text fontWeight={'semibold'} fontSize={"xl"}>Bitcoin Value Statistics</Text>
+                                <Text fontSize={'sm'}>An overview showing the stats of the Bitcoin</Text>
+                            </Stack>
+                            <HStack justifyContent={'space-between'}>
+                                <Text>Price to USD</Text>
+                                <Text fontWeight={'bold'}>${millify(data.data.coin.price)}</Text>
                             </HStack>
-                        )
-                    })}
+                            <HStack justifyContent={'space-between'}>
+                                <Text>Rank</Text>
+                                <Text fontWeight={'bold'}>{millify(data.data.coin.rank)}</Text>
+                            </HStack>
+                            <HStack justifyContent={'space-between'}>
+                                <Text>All Time High</Text>
+                                <Text fontWeight={'bold'}>{millify(data.data.coin.allTimeHigh.price)}</Text>
+                            </HStack>
+
+                            <HStack justifyContent={'space-between'}>
+                                <Text>Market Cap</Text>
+                                <Text fontWeight={'bold'}>{millify(data.data.coin.marketCap)}</Text>
+                            </HStack>
+                            <HStack justifyContent={'space-between'}>
+                                <Text>Number of Exchanges</Text>
+                                <Text fontWeight={'bold'}>{millify(data.data.coin.numberOfExchanges)}</Text>
+                            </HStack>
+                        </Stack>
+                    </VStack>
+
+                </GridItem>
+                <GridItem colSpan={1}>
+
+                    <VStack>
+                        <Stack gap={4}>
+                            <Stack gap={0} py={1}>
+                                <Text fontSize={'xl'} fontWeight='semibold'>Other Statistics of {data.data.coin.name}</Text>
+                                <Text fontSize={'sm'} >An overview showing the stats of the Bitcoin</Text>
+
+                            </Stack>
+
+                            <HStack justifyContent={'space-between'}>
+                                <Text>Number of Markets</Text>
+                                <Text fontWeight={'bold'}>{millify(data.data.coin.numberOfMarkets)}</Text>
+                            </HStack>
+                            <HStack justifyContent={'space-between'}>
+                                <Text>Confirmed Supply</Text>
+                                <Text fontWeight={'bold'}>{data?.data.coin.supply.confirmed ? <div>Yes</div> : <div>No</div>}</Text>
+                            </HStack>
+                            <HStack justifyContent={'space-between'}>
+                                <Text>Circulating Supply</Text>
+                                <Text fontWeight={'bold'}>{millify(data.data.coin.supply.circulating)}</Text>
+                            </HStack>
+                            <HStack justifyContent={'space-between'}>
+                                <Text>Total Supply</Text>
+                                <Text fontWeight={'bold'}>{data?.data.coin.supply.total ? millify(data?.data.coin.supply.total) : <div>NULL</div>}</Text>
+                            </HStack>
+                        </Stack>
+                    </VStack>
+
+                </GridItem>
+                <GridItem >
+                    <VStack>
+                        <Stack pl={['', '', '', '32']} fontSize={'sm'} >
+                            <Text fontSize={'xl'} fontWeight={'semibold'}>What is {data.data.coin.name}?</Text>
+                            <Text className='primary'
+                            >{HTMLReactParser(data.data.coin.description)}
+                            </Text>
+
+                        </Stack>
+                    </VStack>
+                </GridItem>
+                <VStack >
+                    <Stack >
+                        <Text fontSize={'xx-large'} fontWeight={"semibold"}>{data.data.coin.name} Links</Text>
+
+                        {data?.data.coin.links.map((link: any) => {
+                            return (
+                                <Box _hover={{
+                                    backgroundColor: 'gray.100'
+
+                                }}>
+                                    <HStack gap={12} fontWeight={'semibold'} key={link.name} justifyContent={'space-between'}>
+                                        <Text>{link.type}</Text>
+                                        <Link textColor={'blue.400'} href={link.url} isExternal>
+                                            {link.name} <ExternalLinkIcon mx='2px' />
+                                        </Link>
+                                    </HStack>
+                                    <Divider h={4} />
+                                </Box>
+                            )
+                        })}
 
 
-                </Stack>
-            </Stack>
+                    </Stack>
+                </VStack>
+            </Grid>
             <Cryptochart history={history} price={data.data.coin.price} name={data.data.coin.name} />
         </Box>
     )
