46b15fee1fe1fc73134826653935931d9100ee6f
surendhar-palanisamy <surendhar2k18@gmail.com>
2022-07-27 18:12:23 +0530

    responsive hp

--- 2022/goldfit/components/Home/HeroBanner.tsx
+++ 2022/goldfit/components/Home/HeroBanner.tsx
@@ -39,7 +39,10 @@ function HeroBanner() {
                     </Box>
                 </Grid>
             </Grid> */}
-            <Box>
+            <Box
+                sx={{
+
+                }}>
                 <Typography
 
                     sx={{
@@ -50,25 +53,66 @@ function HeroBanner() {
                 >Fitness Club</Typography>
 
                 <Typography fontWeight={700} sx={{
-                    fontSize: '30px'
+                    fontSize: '30px',
+                    py: {
+                        lg: '5px'
+                    }
                 }}>
                     Sweat,Smile <br /> and Repeat
                 </Typography>
-                <Typography>Check out the most effective exercises</Typography>
+                <Typography py={2}>Check out the most effective exercises</Typography>
                 <Button variant="contained" color='error' sx={{
-                    marginTop: '20px'
+                    my: {
+                        lg: '3px'
+                    }
 
                 }}>Explore Exercises</Button>
             </Box>
             <Box sx={{
                 position: 'absolute',
                 top: '0',
-                right: '0',
-                mt: '-49px'
-            }} >
-                <Image height={700} width={600} src={HeroBannerImage} />
-            </Box>
+                right: '-10px',
+                mt: '-49px',
+                height: {
+                    lg: '600px',
 
+                    md: '500px',
+                    sm: '400px'
+
+                },
+                width: {
+                    lg: '500px',
+
+                    md: '500px',
+                    sm: '400px'
+                },
+                display: {
+                    lg: 'flex',
+                    md: 'flex',
+                    sm: 'flex',
+                    xs: 'none'
+                }
+            }} >
+                <Image src={HeroBannerImage} />
+            </Box>
+            <Typography sx={{
+                opacity: 0.1,
+                display: {
+                    lg: 'flex',
+                    md: 'flex',
+                    sm: 'flex',
+                    xs: 'none'
+
+                },
+                p: 0,
+                m: 0,
+                fontSize: {
+                    lg: '200px',
+                    md: '170px',
+                    sm: '50px'
+
+                }
+            }} color='#ff2625'>Exercises</Typography>
         </Box>
     )
 }
--- 2022/goldfit/components/Home/SearchExercise.tsx
+++ 2022/goldfit/components/Home/SearchExercise.tsx
@@ -1,8 +1,45 @@
+import { Box, Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 function SearchExercise() {
     return (
-        <div>SearchExercise</div>
+        <Box>
+            <Stack
+                sx={{
+
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <Typography
+
+                    sx={{
+
+                        fontWeight: '700',
+                        fontSize: { lg: 32 },
+                    }}
+                >Awesome Exercise You
+                </Typography>
+                <Typography
+
+                    sx={{
+
+                        fontWeight: '700',
+                        fontSize: { lg: 32 },
+                    }}
+                >
+                    Should Know</Typography>
+
+
+                <TextField sx={{
+                    width: '100%'
+
+                }} placeholder='Search Exercises'>
+
+                </TextField>
+
+            </Stack>
+        </Box>
     )
 }
 
