2932bc8650aa651c861b6297f57b145e9c13f5df
surendhar-palanisamy <surendhar2k18@gmail.com>
2022-08-05 12:31:55 +0530

    pre schedule

--- lanced/scheduler/backend/base/models.py
+++ lanced/scheduler/backend/base/models.py
@@ -1,4 +1,5 @@
 
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
@@ -10,6 +11,8 @@ from django.db.models.deletion import CASCADE
 class Team(models.Model):
     team_name = models.CharField(max_length=25, blank=True, null=True)
     team_id =  models.CharField(blank=True,null=True,max_length=100)
+    def __str__(self):
+        return str(self.team_name)
 
    
 
@@ -23,7 +26,7 @@ class Profile(models.Model):
     mobile_number = models.BigIntegerField(blank=True,null=True)
     office_number = models.BigIntegerField(blank=True,null=True)
     home_number = models.BigIntegerField(blank=True,null=True)
-    team = models.ManyToManyField(Team)
+    team = models.ManyToManyField(Team,blank=True)
     #member_type_field(manager,leader,member)
     def __str__(self):
         return str(self.user)
@@ -36,14 +39,35 @@ class Profile(models.Model):
 
 
 class AssignedRole(models.Model):
-    Role_CHOICES =[
-        ('primary','primary'),
-        ('secondary','secondary'),
-        ('teritary','teritary')
+    Role_CHOICES =[   
+        ('Member','Member'),
+        ('Primary member','Primary member'),
+        ('Secondary member','Secondary member'),
+        ('Teritary member','Teritary member'),
+        ('Primary lead','Primary lead'),
+        ('Secondary lead','Secondary lead'),
+        ('Manager','Manager')
     ]
-    team_id = models.ManyToManyField(Team)
-    user_id = models.ManyToManyField(User)
+    team_id = models.ForeignKey(Team,on_delete=CASCADE,related_name='assigned_team_id') #primary key
+    user_id = models.ForeignKey(User,on_delete=CASCADE,related_name='assigned_user_id') #primary key
     roles = models.CharField(max_length=60,choices=Role_CHOICES)
+    # def __str__(self):
+    #     return str(self.roles)
+
+
+
+#meetings assigned
+#team_id
+#time
+#roles
+
+class MeetingsAssigned (models.Model):
+    date = models.DateField(blank=True,null=True)
+    time = models.TimeField(blank=True,null=True)
+    date_time = models.DateTimeField(blank=True,null=True)
+    team_id = models.ForeignKey(Team,on_delete=CASCADE,related_name='meeting_team_id')
+    roles = models.ManyToManyField(AssignedRole,blank=True)
+
 
 
 
