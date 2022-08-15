db77cb2fbf23ab41d5de49079d0ab9c99af6f932
surendhar-palanisamy <surendhar2k18@gmail.com>
2022-08-15 22:55:01 +0530

    buttons

--- 2022/goldfit/components/Home/Exercises.tsx
+++ 2022/goldfit/components/Home/Exercises.tsx
@@ -1,48 +1,86 @@
-import { Typography,Box, Grid, Button } from '@mui/material'
+import { Typography,Box, Grid, Button, ButtonBase, Stack, Pagination } from '@mui/material'
 
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 function Exercises({data}:any) {
     // console.log(data,'data is printed from exercises')
+    const [currentpage,setcurrentpage]= useState(1)
+    const paginate = (e:any,value:any)=>{
+        setcurrentpage(value)
+        console.log('paginate clicked')
+    }
     return (
         <Box bgcolor={''}>
 <Typography pt={4} fontSize={"40px"} pb={4} fontWeight={'bold'}>Showing Results</Typography>
-<Grid container spacing={1}>
+<Grid container spacing={0}>
 {data.map((exercise:any,id:any)=>{
     return (
-       <Grid item xs={6}>
+       <Grid item xs={6} sx={{
+        
+    }}>
          <Box  border={3}
         borderBottom={0}
         borderLeft={0}
         borderRight={0}
         borderColor="red"
-        mb={2}
+        mb={1}
         mx={20}
-        my={2}
+        mt={2}
+        sx={{
+            transition: "transform 0.30s ease-in-out",
+            "&:hover":{transform: "scale3d(1.1, 1.1, 1.1)" },
+            my:3
+        }}
          key={exercise.name}
          >
         <Box><Image width={500} height={500} src={exercise.gifUrl} loading='lazy'/></Box>
 
-       <Button
-        sx={{
-        borderRadius:24,
-        px:2,
-        py:0.5,
-        backgroundColor:'#F9EB9D'
+      <Stack columnGap={1} sx={{
+        flexDirection:'row',
         
-       }}
-   
-       variant="contained"> 
-       {exercise.bodyPart}
-       </Button>
-         <Button>{exercise.target}</Button>
-        <Typography> { exercise.name}</Typography>
+      }}>
+      <Button
+  
+  sx={{
+color:'white',
+background:'#ffa9a9',
+borderRadius:'20px',
+textTransform:'capitalize',
+'&:hover':{
+  background:'#ffa9a9'
+}
+ }}
+
+ > 
+ {exercise.bodyPart}
+ </Button>
+ <Button
+
+sx={{
+color:'white',
+background:'#fcc757',
+borderRadius:'20px',
+textTransform:'capitalize',
+
+'&:hover':{
+background:'#fcc757'
+}
+}}
+
+> {exercise.target}</Button>
+      </Stack>
+        <Typography sx={{textTransform:'uppercase',fontWeight:600,mt:2,fontSize:15}}> { exercise.name}</Typography>
 
         </Box>
        </Grid>
     )
 })}
+<Box
+display={'flex'} 
+alignItems="center"
+ justifyContent="center"><Pagination count={Math.ceil(data.length/9)} onChange={()=>{paginate}}  page={currentpage}/></Box>
+ 
 </Grid>
 
 
