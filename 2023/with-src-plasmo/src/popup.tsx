d1b3d364dfab92bb62ad0b8559c805c4f5ab7b19
Surendhar <surendhar2k18@gmail.com>
2023-02-25 14:53:04 +0530

    plasmo with src folder

---
+++ 2023/with-src-plasmo/src/popup.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+
+function IndexPopup() {
+  const [data, setData] = useState<string>("")
+
+  const setUrl = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      var currentUrl = tabs[0].url
+      setData(currentUrl)
+    })
+  }
+
+  useEffect((): void => {
+    setUrl()
+  }, [])
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
+      {data}
+    </div>
+  )
+}
+
+export default IndexPopup
