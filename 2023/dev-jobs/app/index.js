762c8a938385f976967adc0bcdd72f9f0ace13c9
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2023-05-06 21:43:12 +0530

    test expo router

---
+++ 2023/dev-jobs/app/index.js
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import { Link, Stack } from "expo-router";
+
+export default function Home() {
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            {/* Use the `Screen` component to configure the layout. */}
+            <Stack.Screen options={{ title: "Overview" }} />
+            {/* Use the `Link` component to enable optimized client-side routing. */}
+            <Link href="/details">Go to Details</Link>
+            <Text>Beach the hot reload is so cool</Text>
+            <Text>Woow😎</Text>
+        </View>
+    );
+}
