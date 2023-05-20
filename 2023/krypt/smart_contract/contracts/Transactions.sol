75c1f85b3fce69ca56ac8be51f28d841f1ea2a2a
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2023-05-20 23:53:37 +0530

    add Transfer data type

--- 2023/krypt/smart_contract/contracts/Transactions.sol
+++ 2023/krypt/smart_contract/contracts/Transactions.sol
@@ -1,7 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-
 contract Transactions {
     uint256 transactionCounter;
+    event Transfer(
+        address sender,
+        address receiver,
+        uint amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+    struct TransferStruct{
+         address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
 }
