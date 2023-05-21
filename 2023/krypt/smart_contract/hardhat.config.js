c27d51eb1cdd7fb74a7dd3cc790dfcd6705e632e
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2023-05-21 16:45:09 +0530

    deploy via alchemy+sepolia+hardhat

--- 2023/krypt/smart_contract/hardhat.config.js
+++ 2023/krypt/smart_contract/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv')
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const APP_URL = process.env.APP_URL
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
+
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/G2mZbNMDezU55rm8RcRWgLLkjwPkO5gh`,
+      accounts: [`715bc7236c3055d52f15fb9ce99cc5d70862805b30d10e04c13b303eb62c3e56`]
+    }
+  }
 };
