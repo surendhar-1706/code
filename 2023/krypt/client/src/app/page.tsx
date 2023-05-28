9becc3c2fc7f31c0fe024151c2e6eba97c571a85
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2023-05-28 16:24:18 +0530

    send eth via form

--- 2023/krypt/client/src/app/page.tsx
+++ 2023/krypt/client/src/app/page.tsx
@@ -2,12 +2,13 @@
 import SendEthForm from "@/components/SendEthForm";
 import { MetaMaskInpageProvider } from "@metamask/providers";
 import { contractAbi, contractAddress } from "@/utils/constant";
-import { ethers } from "ethers";
-import { useEffect } from "react";
+import { ethers, toBigInt } from "ethers";
+import { useEffect, useState } from "react";
 import { useAtom } from "jotai";
 import { accountAtom } from "@/atoms/app-atoms";
 import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
+import { sendEthFormType } from "@/types/formtypes";
 // declare var window: any;
 declare global {
   interface Window {
@@ -17,12 +18,14 @@ declare global {
 
 export default function Home() {
   const { ethereum } = window;
+  const [isLoading, setIsLoading] = useState(false);
   const [userAccounts, setUserAccounts] = useAtom(accountAtom);
   const getEtherumcontract = async () => {
     const provider = new ethers.BrowserProvider(ethereum);
     const signer = await provider.getSigner();
     const contract = new ethers.Contract(contractAddress, contractAbi, signer);
-    console.log(provider, signer, contract);
+    console.log("getEthereum Contract called!!!");
+    return contract;
   };
 
   const checkIfWallectConnected = async () => {
@@ -48,21 +51,52 @@ export default function Home() {
       throw new Error("No ether object found");
     }
   };
+  const sendTransaction = async (formData: sendEthFormType) => {
+    if (!ethereum) return "Please Intstall MetaMask!!!";
+    console.log("hi!!!!");
+    const { ethAmount, gifKeyword, message, toAddress } = formData;
+    const transactionContract = getEtherumcontract();
+    const parsedAmount = ethers.parseEther(ethAmount);
+    //@ts-ignore
+    await ethereum.request({
+      method: "eth_sendTransaction",
+      params: [
+        {
+          from: userAccounts,
+          to: toAddress,
+          gas: "0x5208",
+          amount: parsedAmount,
+        },
+      ],
+    });
+    //@ts-ignore
+    // const transactionHash = await transactionContract.addToBlockChain(
+    //   toAddress,
+    //   parsedAmount,
+    //   message,
+    //   gifKeyword
+    // );
+    // await transactionHash.wait();
+  };
   useEffect(() => {
-    // getEtherumcontract();
     checkIfWallectConnected();
   }, []);
   return (
     <div className="h-screen bg-white">
       <div className="flex mb-10 items-center justify-between px-20 pt-4 pb-4 border-b">
         <div className="text-black text-xl">Krypt</div>
-        <AwesomeButton ripple={true} onPress={connnectWallet}>
-          Connect Wallet
+        <AwesomeButton
+          className={userAccounts ? "pointer-events-none" : "cursor-pointer"}
+          ripple={true}
+          onPress={connnectWallet}
+          type="primary"
+        >
+          {userAccounts ? "Wallet Connected" : "Connect Wallet"}
         </AwesomeButton>
       </div>
       <div className="grid place-items-center h-4/6">
         {" "}
-        <SendEthForm />
+        <SendEthForm sendTransaction={sendTransaction} />
       </div>
     </div>
   );
--- 2023/krypt/client/src/components/SendEthForm.tsx
+++ 2023/krypt/client/src/components/SendEthForm.tsx
@@ -4,44 +4,53 @@ import { sendEthFormType } from "@/types/formtypes";
 import { useAtom } from "jotai";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-export default function SendEthForm() {
+type sendEthFormProps = {
+  sendTransaction: (formData: sendEthFormType) => Promise<string | undefined>;
+};
+
+export default function SendEthForm({ sendTransaction }: sendEthFormProps) {
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm<sendEthFormType>();
-  const onSubmit: SubmitHandler<sendEthFormType> = (data) => console.log(data);
-  const [connectedUserAccount] = useAtom(accountAtom);
+  const onSubmit: SubmitHandler<sendEthFormType> = async (data) => {
+    console.log(data);
+    const res = await sendTransaction(data);
+    console.log(res);
+  };
 
   return (
     <form
-      className="flex flex-col px-10 py-8 bg-white rounded-lg shadow-lg w-80 bg-opacity-20 backdrop-filter backdrop-blur-lg border-opacity-10 gap-y-8 placeholder:text-xs"
+      className="flex flex-col px-10 py-8 bg-white text-black rounded-lg shadow-lg w-80 bg-opacity-20 backdrop-filter backdrop-blur-lg border-opacity-10 gap-y-8 placeholder:text-xs"
       onSubmit={handleSubmit(onSubmit)}
     >
       <input
         className="outline-none rounded border-[0.2px] py-1 px-2 placeholder:text-xs placeholder:text-gray-300"
         placeholder="Address to"
         defaultValue=""
-        {...register("toAddress")}
+        {...register("toAddress", { required: false })}
       />
       <input
         className="outline-none rounded border-[0.2px] py-1 px-2 placeholder:text-xs placeholder:text-gray-300"
         placeholder="Amount Eth"
-        defaultValue=""
-        {...register("ethAmount")}
+        defaultValue={""}
+        type="number"
+        step={0.0001}
+        {...register("ethAmount", { required: false })}
       />
       <input
         className="outline-none rounded border-[0.2px] py-1 px-2 placeholder:text-xs placeholder:text-gray-300"
         placeholder="Keyworld (Gif)"
         defaultValue=""
-        {...register("gifKeyword")}
+        {...register("gifKeyword", { required: false })}
       />
       <input
         className="outline-none rounded border-[0.2px] py-1 px-2 placeholder:text-xs placeholder:text-gray-300"
         placeholder="Enter Message"
         defaultValue=""
-        {...register("message")}
+        {...register("message", { required: false })}
       />
       <button
         className="py-2 text-white transition-colors duration-300 bg-teal-400 border ease-intext-xs hover:bg-teal-500 rounded-xl"
