18af11b16099ed1302833b2e02d73938da46bef1
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2023-06-04 01:54:50 +0530

    create contract on Add

--- 2023/tasklist/fe/src/App.jsx
+++ 2023/tasklist/fe/src/App.jsx
@@ -1,16 +1,68 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect } from "react";
 import "./App.css";
-import { Button } from "@chakra-ui/react";
-
+import { Box, Button, Card, HStack, Input, VStack } from "@chakra-ui/react";
+import { ethers } from "ethers";
+import { useState } from "react";
+import { contractAddress } from "./utils/config";
+import contractAbi from "../src/utils/TaskContract.json";
 function App() {
-  const [count, setCount] = useState(0);
+  const { ethereum } = window;
+  const [userActiveAccount, setUserActiveAccount] = useState("");
+  const [formData, setFormData] = useState();
+  const checkMetaMaskInstalled = () => {
+    if (!ethereum) {
+      alert("Install MetaMask!!");
+    }
+  };
+  const fetchChainIdAndCheckSepolia = async () => {
+    checkMetaMaskInstalled();
+    try {
+      const chainId = await ethereum.request({ method: "eth_chainId" });
 
+      if (chainId !== "0xaa36a7") {
+        alert("Change to Sepolia!!");
+      } else {
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setUserActiveAccount(accounts[0]);
+        console.log(accounts[0]);
+      }
+    } catch (error) {
+      console.error("Error fetching chainId or accounts:", error);
+    }
+  };
+
+  const addTask = async () => {
+    if (ethereum) {
+      const provider = new ethers.BrowserProvider(ethereum);
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractAbi.abi,
+        signer
+      );
+      const response = await contract.addTask(formData);
+      console.log(response);
+    }
+  };
+  useEffect(() => {
+    fetchChainIdAndCheckSepolia();
+  }, []);
   return (
-    <>
-      <Button>hi</Button>
-    </>
+    <VStack rowGap={"40"}>
+      <HStack>
+        <Input
+          name="addTask"
+          onChange={(e) => {
+            setFormData(e.target.value);
+          }}
+        />
+        <Button onClick={addTask}>Add Task</Button>
+      </HStack>
+      <Box>{formData}</Box>
+      <Card p={"2"}>Connected Address : {userActiveAccount}</Card>
+    </VStack>
   );
 }
 
--- 2023/tasklist/fe/src/utils/config.js
+++ 2023/tasklist/fe/src/utils/config.js
@@ -1,4 +1,4 @@
 export const apiUrl = ''
 export const privateKey = ''
-export const contractAddress = ''
+export const contractAddress = '0x267379a10C62f60da3504F963e05D4535E508d7E'
 export const contractAbi = ''
