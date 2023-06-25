3bf93a27ed6167291ddc6f196fe21fa89e9811ae
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2023-06-25 16:44:54 +0530

    finish the contract

--- 2023/crowdfunding/crowdfunding-contract/contracts/CrowdFunding.sol
+++ 2023/crowdfunding/crowdfunding-contract/contracts/CrowdFunding.sol
@@ -22,8 +22,50 @@ contract CrowdFunding {
         string memory _description,
         uint _target,
         uint _deadline,
-        string memory image
-    ) public {}
+        string memory _image
+    ) public returns (uint) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+        require(
+            campaign.deadline < block.timestamp,
+            "The deadline should be a date in the future"
+        );
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+        campaign.deadline = _deadline;
+        numberOfCampaigns++;
+        return numberOfCampaigns - 1;
+    }
 
-    constructor() {}
+    function donateToCampaign(uint _id) public payable {
+        uint amount = msg.value;
+        Campaign storage campaign = campaigns[_id];
+        campaign.donors.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonors(
+        uint _id
+    ) public view returns (address[] memory, uint[] memory) {
+        return (campaigns[_id].donors, campaigns[_id].donations);
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for (uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign memory item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+        return allCampaigns;
+    }
+    // constructor() {}
 }
