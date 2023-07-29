c276028700815311b8bd41d1912e88624eca6dc4
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2023-07-29 18:32:38 +0530

    created seperate frontend and backend image

--- 2023/devops/dj_react_docker_postgres/backend/api/api/settings.py
+++ 2023/devops/dj_react_docker_postgres/backend/api/api/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'django-insecure-6%dob)$$i4wamon5+i$yxccy=+@%8z+=@fy0750ks8y)u$3zaq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -37,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'crud',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- 2023/devops/dj_react_docker_postgres/backend/api/crud/views.py
+++ 2023/devops/dj_react_docker_postgres/backend/api/crud/views.py
@@ -1,3 +1,4 @@
+import logging
 from rest_framework import viewsets
 from crud.seriallizers import TodoSerializer
 from crud.models import Todo
@@ -7,6 +8,9 @@ class TodoViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and editing user instances.
     """
-
-    serializer_class = TodoSerializer
-    queryset = Todo.objects.all()
+    try:
+        logging.error('-----------')
+        serializer_class = TodoSerializer
+        queryset = Todo.objects.all()
+    except Exception as e:
+        logging.error(e)
